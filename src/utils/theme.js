74f8e9fce5fd3a2740df0f0257955c36e3f295e5
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-09-05 17:54:02 +0300

    added theme provider

---
+++ src/utils/theme.js
@@ -0,0 +1,34 @@
+export const theme = {
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+
+  fontSizes: { xs: 12, s: 14, m: 16, l: 20, xl: 24, xxl: 32, xxxl: 48 },
+
+  fontWeights: {
+    body: 400,
+    bold: 700,
+  },
+
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#07c',
+    secondary: '#30c',
+    muted: '#f6f6f6',
+  },
+
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+
+  radii: {
+    none: '0',
+    normal: '4px',
+    round: '50%',
+  },
+};
